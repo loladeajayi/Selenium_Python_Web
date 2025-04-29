9eec8d3ba73ffb0efe78cb9c8f518c920552e90e
Ololade Grace Ajayi <ololadegrace.ajayi@gmail.com>
2025-04-28 23:25:12 -0400

    Update conftest and Github Actions Workflow

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture(scope="function")
+def driver():
+    options = Options()
+    options.add_argument("--start-maximized")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")  
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
